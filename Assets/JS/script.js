a8724976da9b8f19b3140c083f803ffd4f26d8c7
Oyinloye Joe <oyinjoe23@gmail.com>
2022-04-09 00:34:24 -0700

    home section

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,1 +1,20 @@
-con;
+//-------------- MENU SHOW Y HIDDEN---------------//
+const navMenu = document.getElementById("nav__menu"),
+  navClose = document.getElementById("nav__close"),
+  navToggle = document.getElementById("nav__toggle");
+
+//---------- MENU SHOW---------------------//
+// Validate if constant exists//
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+//---------- MENU HIDDEN --------------//
+// Validate if constant exists//
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
